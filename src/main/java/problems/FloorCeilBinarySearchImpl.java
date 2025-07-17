e3f55127cf980e31652c1b8b0560968ef8c048bc
124110621 BIANKA VITORIA SILVA LIMA <bianka.vitoria.silva.lima@lcc1-42.lcc.ufcg.edu.br>
2025-07-17 09:41:36 -0300

    atual

--- src/main/java/problems/FloorCeilBinarySearchImpl.java
+++ src/main/java/problems/FloorCeilBinarySearchImpl.java
@@ -32,10 +32,62 @@ public class FloorCeilBinarySearchImpl implements FloorCeil {
 		return null;
 	}
 
+	public Integer binaryFloor(Integer[] array, Integer x, int leftIndex, int rightIndex, int diferença) {
+		if ((array != null) && (array.length > 0) && (leftIndex < rightIndex)) {
+			int meio = ((rightIndex - leftIndex) / 2) + leftIndex;
+			while (diferença >= 0) {
+				if (diferença == 0) {
+					return x;
+				} else if (((x - array[meio]) < diferença) && diferença >= 0) {
+					diferença = x - array[meio];
+				}
+			}
+			
+		}
+		return null;
+	}
+
 	@Override
 	public Integer ceil(Integer[] array, Integer x) {
 		// TODO implement your code here
 		throw new UnsupportedOperationException("Not implemented yet!");
 	}
+
+
+
+	
+	public Integer menorIgual(Integer[] array, Integer x) {
+		
+		return null;
+	}
+
+	public Integer menorIgualLogic(Integer[] array, Integer x, int leftIndex, int rightIndex, int dif) {
+		int floor = array[leftIndex];
+		dif = x;
+		if ((array != null) && (array.length > 0) && (leftIndex < rightIndex)) {
+			int meio = ((((rightIndex - leftIndex) / 2) + leftIndex));
+			if (array[meio] == x) {
+				return x;
+			} else if (array[meio] < x && (((x - array[meio]) < dif) && (x - array[meio]) > 0)) {
+				dif = x - array[meio];
+				menorIgualLogic(array, x, leftIndex, meio - 1, dif);
+			} else {
+				menorIgualLogic(array, x, meio + 1, rightIndex, dif);
+			}
+			
+		} else if((array != null) && (array.length > 0) && (leftIndex >= rightIndex)) {
+			
+			floor = x - dif;
+
+			if (floor <= x)
+				return floor;
+
+		}
+		
+		
+		return null;
+	}
+
+
 
 }
